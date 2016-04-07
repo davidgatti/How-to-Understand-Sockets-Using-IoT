834a72e7dd78bf10292f9bcb39a3cfec858e23ed
David Gatti <office@gatti.pl>
2016-04-07 22:14:34 +0200

    Removed unnecessary code - lots of it :D

--- Examples/BigMessageParticle2NodeJS/tcpParticleClient.cpp
+++ Examples/BigMessageParticle2NodeJS/tcpParticleClient.cpp
@@ -29,64 +29,22 @@ void setup() {
 void loop() {
 
     int txtSize = strlen(txt);  // Get the size of our data
-    int position = 0;           // Set the starting position
-    int bufSize = 1024;         // Set our buffer size
 
-    // We are going to chop our data if it is to big
-    // so we can put it all together on the other end
-    while(txtSize) {
+    // Create a string so we can use it as our buffer
+    unsigned char buf[txtSize];
 
-        // Remove the buffer size that we wen thru
-        int next = txtSize - bufSize;
+    // Convert char array pointer in to a nice string
+    for(int c=0; c<= txtSize; c++) {
 
-        // Check if we are in the last iteration. If so, we have to set
-        // the remaining buffer size which might be smaller then our buffer size.
-        if(next <= 0) {
-            bufSize = txtSize;
-        } else {
-            txtSize = next;
-        }
-
-        // Initialize a char pointer array each time we want to store a fragment of our data.
-        // The size of the array needs to be dynamic, because our data chunks might be smaller
-        // towards the end.
-        char* tmp = (char*) malloc(bufSize);
-
-        // Red a fragment of the data.
-        for(int x=0; x < bufSize; x++) {
-
-            tmp[x] = txt[position++];
-
-        }
-
-        // Create a string so we can use it as our buffer
-        unsigned char buf[bufSize];
-
-        // Convert char array pointer in to a nice string
-        for(int c=0; c<= bufSize; c++) {
-
-            buf[c] = tmp[c];
-
-        }
-
-        // Send our data to the remote server
-        client.write(buf, bufSize);
-
-        // Free up memory so we can set our variable again with a different size
-        free(tmp);
-
-        // If we are in the last iteration, then we send a END character.
-        // Since there is no way of controlling how the data will be sent
-        // (in how many peaces), we need a way of telling if we received
-        // all of our data.
-        if(next <= 0) {
-
-            client.print(',');
-            break;
-
-        }
+        buf[c] = txt[c];
 
     }
+
+    // Send our data to the remote server
+    client.write(buf, txtSize);
+
+    // Send the separator
+    client.print(',');
 
     Serial.println("Done");
 
